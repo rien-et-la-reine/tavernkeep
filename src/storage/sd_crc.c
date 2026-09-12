be5492d805350af4a39d2b759f4c4d8d30bfadbb
Harsh Patel <harshnp02@gmail.com>
2026-09-12 10:55:54 -0400

    rolling crc 16
    created a rolling variant of the crc16 helper, currently not used, potentially will replace the all-at-once crc16 helper in the future

--- src/storage/sd_crc.c
+++ src/storage/sd_crc.c
@@ -71,3 +71,27 @@ uint16_t crc_helper_16(const uint8_t *message, size_t length) {
     //CRC value (return raw CRC value, processing for transmission should happen in context, not here)
     return eval;
 }
+
+//CRC16 Rolling
+bool crc_helper_rolling_16(uint16_t crc, uint8_t data) {
+    //G(x) = x^16 + x^12 + x^5 + 1 -> mask: 0x1021
+    uint16_t mask = 0x1021;
+    //feedback bool to gate whether XOR operation occurs
+    bool feedback;
+    //bit number of next bit
+    uint8_t next_bit = 7;
+
+    do {
+        //calculate feedback from existing crc topbit and incoming data bit, then decrement next_bit
+        feedback = ((crc >> 15) & 1) ^ ((data >> next_bit--) & 1);
+        //shift working register
+        crc = crc << 1;
+        //if feedback is true, bitwise XOR with mask
+        if (feedback) {
+            crc ^= mask;
+        }
+    //repeat for each bit of data byte
+    } while (next_bit >= 0);
+    //return new rolling crc value
+    return crc;
+}
--- src/storage/sd_crc.h
+++ src/storage/sd_crc.h
@@ -5,3 +5,5 @@
 uint8_t crc_helper_7(const uint8_t *message, size_t length);
 //CRC16
 uint16_t crc_helper_16(const uint8_t *message, size_t length);
+//CRC16 Rolling
+bool crc_helper_rolling_16(uint16_t crc, uint8_t data);
